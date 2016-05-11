86760f51e72d98b9dc834892a3a9e427353ff0be
Simon Jakobi <simon.jakobi@gmail.com>
2016-05-11 21:21:16 +0200

    Add first cereal results

--- README.md
+++ README.md
@@ -13,20 +13,22 @@ stack build --exec string-map-binary-comparison
 ## Results
 
 ```
-+----++-----------------------------------------+-----------------+---------------+
-|    ||                                    Type |         Package | Size in Bytes |
-+====++=========================================+=================+===============+
-|  1 ||                            StringMap () |    TernaryTrees |        123858 |
-|  2 ||                   TernaryMap [Word8] () |    TernaryTrees |        123858 |
-|  3 ||                            StringMap () |  data-stringmap |        147083 |
-|  4 ||                           Map String () |      containers |        185083 |
-|  5 ||                          [(String, ())] |            base |        185083 |
-|  6 ||                                 Trie () | bytestring-trie |        220212 |
-|  7 ||         PatriciaTrieMap AList String () |      list-tries |        277307 |
-|  8 ||           PatriciaTrieMap Map String () |      list-tries |        277307 |
-|  9 || PatriciaTrieMap WrappedIntMap String () |      list-tries |        277307 |
-| 10 ||             ListTrieMap AList String () |      list-tries |        570899 |
-| 11 ||               ListTrieMap Map String () |      list-tries |        570899 |
-| 12 ||     ListTrieMap WrappedIntMap String () |      list-tries |        570899 |
-+----++-----------------------------------------+-----------------+---------------+
++----++-----------------------------------------+-----------------+-----------------------+---------------+
+|    ||                                    Type |         Package | Serialization Library | Size in Bytes |
++====++=========================================+=================+=======================+===============+
+|  1 ||                            StringMap () |    TernaryTrees |                binary |        123858 |
+|  2 ||                   TernaryMap [Word8] () |    TernaryTrees |                binary |        123858 |
+|  3 ||                            StringMap () |  data-stringmap |                binary |        147083 |
+|  4 ||                           Map String () |      containers |                binary |        185083 |
+|  5 ||                          [(String, ())] |            base |                binary |        185083 |
+|  6 ||                           Map String () |      containers |                cereal |        185083 |
+|  7 ||                          [(String, ())] |            base |                cereal |        185083 |
+|  8 ||                                 Trie () | bytestring-trie |                binary |        220212 |
+|  9 ||         PatriciaTrieMap AList String () |      list-tries |                binary |        277307 |
+| 10 ||           PatriciaTrieMap Map String () |      list-tries |                binary |        277307 |
+| 11 || PatriciaTrieMap WrappedIntMap String () |      list-tries |                binary |        277307 |
+| 12 ||             ListTrieMap AList String () |      list-tries |                binary |        570899 |
+| 13 ||               ListTrieMap Map String () |      list-tries |                binary |        570899 |
+| 14 ||     ListTrieMap WrappedIntMap String () |      list-tries |                binary |        570899 |
++----++-----------------------------------------+-----------------+-----------------------+---------------+
 ```
--- src/Comparison.hs
+++ src/Comparison.hs
@@ -18,6 +18,7 @@ import qualified Data.ListTrie.Patricia.Map as Patricia
 import qualified Data.Map as Map
 import qualified Data.Map.StringMap as TernaryStringMap
 import qualified Data.Map.TernaryMap as TernaryMap
+import qualified Data.Serialize as Serialize
 import qualified Data.StringMap.Strict as StringMap
 import qualified Data.Trie as BSTrie
 import qualified System.IO.Strict as StrictIO
@@ -31,60 +32,84 @@ run = do
       bsMapping = map (first BSChar8.pack) mapping
 
       results =
-        [ Result
+        [ -- binary
+          Result
             "base"
             "[(String, ())]"
+            "binary"
             (encodedSize mapping)
         , Result
             "TernaryTrees"
             "StringMap ()"
+            "binary"
             (encodedSize (TernaryStringMap.fromList mapping))
         , Result
             "TernaryTrees"
             "TernaryMap [Word8] ()"
+            "binary"
             (encodedSize (TernaryMap.fromList (map (first ByteString.unpack) bsMapping)))
         , Result
             "containers"
             "Map String ()"
+            "binary"
             (encodedSize (Map.fromList mapping))
         , Result
             "bytestring-trie"
             "Trie ()"
+            "binary"
             (encodedSize (BSTrie.fromList bsMapping))
         , Result
             "data-stringmap"
             "StringMap ()"
+            "binary"
             (encodedSize (StringMap.fromList mapping))
         , Result
             "list-tries"
             "ListTrieMap WrappedIntMap String ()"
+            "binary"
             (encodedSize
               (ListTrie.fromList mapping :: ListTrie.TrieMap WrappedIntMap Char ()))
         , Result
             "list-tries"
             "ListTrieMap AList String ()"
+            "binary"
             (encodedSize
               (ListTrie.fromList mapping :: ListTrie.TrieMap AList Char ()))
         , Result
             "list-tries"
             "ListTrieMap Map String ()"
+            "binary"
             (encodedSize
               (ListTrie.fromList mapping :: ListTrie.TrieMap Map.Map Char ()))
         , Result
             "list-tries"
             "PatriciaTrieMap WrappedIntMap String ()"
+            "binary"
             (encodedSize (Patricia.fromList mapping :: Patricia.TrieMap WrappedIntMap Char ()))
         , Result
             "list-tries"
             "PatriciaTrieMap AList String ()"
+            "binary"
             (encodedSize (Patricia.fromList mapping :: Patricia.TrieMap AList Char ()))
         , Result
             "list-tries"
             "PatriciaTrieMap Map String ()"
+            "binary"
             (encodedSize (Patricia.fromList mapping :: Patricia.TrieMap Map.Map Char ()))
 
+          -- cereal
+        , Result
+            "base"
+            "[(String, ())]"
+            "cereal"
+            (serializedSize mapping)
+        , Result
+            "containers"
+            "Map String ()"
+            "cereal"
+            (serializedSize (Map.fromList mapping))
         ]
-      table = tabulateResults (sortOn (\(Result p t c) -> (c, t, p)) results)
+      table = tabulateResults (sortOn (\(Result p t l c) -> (c, l, t, p)) results)
 
   outputTable table
 
@@ -94,15 +119,23 @@ outputTable = putStrLn . AsciiArt.render id id id
 data Result a = Result
   { resultPackage :: String
   , resultType :: String
+  , resultSerializationLibrary :: String
   , resultContent :: a
   } deriving (Show, Eq, Ord, Functor)
 
 encodedSize :: Binary.Binary a => a -> Int64
 encodedSize = LazyByteString.length . Binary.encode
 
+serializedSize :: Serialize.Serialize a => a -> Int64
+serializedSize = LazyByteString.length . Serialize.encodeLazy
+
 tabulateResults :: Show a => [Result a] -> Table String String String
 tabulateResults results =
   Table
     (Group NoLine (map (Header . show) [1 .. length results]))
-    (Group SingleLine (map Header ["Type", "Package", "Size in Bytes"]))
-    (map (\(Result p t c) -> [t, p, show c]) results)
+    (Group
+      SingleLine
+      (map
+        Header
+        ["Type", "Package", "Serialization Library", "Size in Bytes"]))
+    (map (\(Result p t l c) -> [t, p, l, show c]) results)
